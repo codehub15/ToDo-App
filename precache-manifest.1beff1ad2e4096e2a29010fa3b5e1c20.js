def6228875b66eb652599982565bfe7fccbe9ca9
natalialexandra <natalia.alexandra195@gmail.com>
2020-03-10 10:27:54 +0100

    Updates

---
+++ precache-manifest.1beff1ad2e4096e2a29010fa3b5e1c20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af238be77cd10ac6425a5b4ad6b12cd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f839ab2c93e0a2e046f2",
+    "url": "./static/css/main.15dfc9c2.chunk.css"
+  },
+  {
+    "revision": "041cbe80e51f3b5753d5",
+    "url": "./static/js/2.da71ded7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.da71ded7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f839ab2c93e0a2e046f2",
+    "url": "./static/js/main.98c875a7.chunk.js"
+  },
+  {
+    "revision": "152acc8b461207c5015b",
+    "url": "./static/js/runtime-main.d46db987.js"
+  }
+]);
